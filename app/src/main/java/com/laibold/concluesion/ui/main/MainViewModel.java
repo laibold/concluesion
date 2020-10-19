115e634526b7cbbab8eb1ed52b63aeed55d50515
Marvin <marvin.laibold@yahoo.de>
2020-10-19 18:49:54 +0200

    Create Layouts and MainViewModel

--- app/src/main/java/com/laibold/concluesion/ui/main/MainViewModel.java
+++ app/src/main/java/com/laibold/concluesion/ui/main/MainViewModel.java
@@ -1,7 +1,19 @@
 package com.laibold.concluesion.ui.main;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.laibold.concluesion.model.Game;
+
 public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private final MutableLiveData<Game> game = new MutableLiveData<>();
+
+    public void setGame(Game game) {
+        this.game.setValue(game);
+    }
+
+    public LiveData<Game> getGame() {
+        return this.game;
+    }
 }
